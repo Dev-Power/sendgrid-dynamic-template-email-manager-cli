eaf807cad7419b0761867b22d50e1dcb62573615
dev <dev@example.com>
2022-07-04 12:15:26 +0100

    initial commit

---
+++ src/DynamicTemplateManager.Cli/Program.cs
@@ -0,0 +1,10 @@
+﻿using CliFx;
+
+await new CliApplicationBuilder()
+    .AddCommandsFromThisAssembly()
+    .SetExecutableName("dtm")
+    .SetDescription("CLI to manage SendGrid Dynamic Email Templates")
+    .SetTitle("SendGrid Dynamic Email Template Manager")
+    .Build()
+    .RunAsync();
+        
